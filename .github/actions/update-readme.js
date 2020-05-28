c07ef32cecf6666940b78a6e627a67cba106b7e2
Jason Etcovitch <jasonetco@github.com>
2020-05-27 22:57:24 -0400

    MAX_ITEMS

--- .github/actions/update-readme.js
+++ .github/actions/update-readme.js
@@ -2,11 +2,15 @@ const { Toolkit } = require('actions-toolkit')
 const Parser = require('rss-parser')
 const parser = new Parser()
 
+const MAX_ITEMS = 30
+
 Toolkit.run(async tools => {
   // Fetch feed
   const feed = await parser.parseURL(process.env.FEED_URL)
   // Create our new README string. This could be way more robust.
-  const newString = feed.items.map(item => `* [${item.title}](${item.link})`).join('\n')
+  const newString = feed.items
+    .slice(0, MAX_ITEMS)
+    .map(item => `* [${item.title}](${item.link})`).join('\n')
   // Update the README.md
   await tools.github.repos.createOrUpdateFile({
     ...tools.context.repo,
@@ -15,5 +19,5 @@ Toolkit.run(async tools => {
     path: 'README.md'
   })
 }, {
-  secrets: ['GITHUB_TOKEN']
+  secrets: ['GITHUB_TOKEN', 'FEED_URL']
 })
