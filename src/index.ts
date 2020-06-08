d6926e284b3ff1733b49253f9c3bfdf531fc181e
Jason Etcovitch <jasonetco@github.com>
2020-06-07 20:46:59 -0400

    Use inputs

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
 import { Toolkit } from 'actions-toolkit'
 import { ReadmeBox } from 'readme-box'
 import Parser from 'rss-parser'
+
 const parser = new Parser()
 
-const MAX_ITEMS = 30
+interface Inputs {
+  'feed-url': string
+  'readme-section': string
+  max: string
+  [key: string]: string
+}
 
-Toolkit.run(async tools => {
+Toolkit.run<Inputs>(async tools => {
   // Fetch feed
-  const feed = await parser.parseURL(process.env.FEED_URL)
+  const feed = await parser.parseURL(tools.inputs['feed-url'])
 
   // Create our new list
   const newString = feed.items
-    .slice(0, MAX_ITEMS)
+    .slice(0, parseInt(tools.inputs.max, 10)) 
     .map(item => `* [${item.title}](${item.link})`).join('\n')
 
   // Update the section of our README
   await ReadmeBox.updateSection(newString, {
     ...tools.context.repo,
     token: tools.token,
-    section: 'feed'
+    section: tools.inputs['readme-section']
   })
-}, {
-  secrets: ['GITHUB_TOKEN', 'FEED_URL']
 })
